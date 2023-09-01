9652b12faeaff071eae7ad73e03e2d2c7deaec94
8200118 <58340504+heyliceeee@users.noreply.github.com>
2023-09-01 18:57:08 +0100

    :sparkles: feat: classe Filme

---
+++ src/main/java/org/example/Movie.java
@@ -0,0 +1,40 @@
+package org.example;
+
+public class Movie {
+    private String title;
+    private String urlImage;
+    private double rating;
+    private int year;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrlImage() {
+        return urlImage;
+    }
+
+    public void setUrlImage(String urlImage) {
+        this.urlImage = urlImage;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
